1ab861de499b8bbf94e2e7150a171449aff2c4b9
dsanderson90 <dsanderson3@cnm.edu>
2019-04-23 19:27:45 -0600

    added prettier

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import styles from './styles/index.css';
 
 const MOUNT_ELEMENT = document.getElementById('root');
 ReactDOM.render(<App />, MOUNT_ELEMENT);
