ba289e7ae1ae0f6d10f45c6523c8a989335590ff
dsanderson90 <dsanderson3@cnm.edu>
2019-04-29 12:40:05 -0600

    wrote tests for each component

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -2,52 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
+import { props } from 'bluebird';
 
 const RecipeDetail = ({ recipe, style }) => {
   if (!recipe) {
     return (
       <p
         style={style}
-        className={'classNames(\'h3 p2 bg-white italic center\'), props.className)'}
+        className={
+          "classNames('h3 p2 bg-white italic center'), props.className)"
+        }
       >
-    Please select a recipe to see the detail.
+        Please select a recipe to see the detail.
       </p>
     );
   }
   return (
-    <div
-      style={style}
-      className="p2 bg-white"
-    >
+    <div style={style} className="p2 bg-white">
       <h2 className="h2">{recipe.name}</h2>
-      <img
-        alt={recipe.name}
-        className="fit"
-        src={recipe.image}
-      />
+      <img alt={recipe.name} className="fit" src={recipe.image} />
       <div>
         <span>{recipe.category}</span>
         <span>{recipe.calories}</span>
       </div>
       <h3>Ingredients:</h3>
-      <ul>
-        {recipe.ingredients.map(ingredient => (
-          <li key={ingredient}>
-            {ingredient}
-          </li>
-        ))}
-      </ul>
+      {recipe.ingredients && (
+        <ul>
+          {recipe.ingredients.map(ingredient => (
+            <li key={ingredient}>{ingredient}</li>
+          ))}
+        </ul>
+      )}
       <h3>Steps:</h3>
-      <ol>
-        {recipe.steps.map(step => (
-          <li key={step}>
-            {step}
-          </li>
-        ))}
-      </ol>
-      <Link
-        to={`/recipe/${recipe.id}`}
-      >
+      {recipe.steps && (
+        <ol>
+          {recipe.steps.map(step => (
+            <li key={step}>{step}</li>
+          ))}
+        </ol>
+      )}
+      <Link to={`/recipe/${recipe.id}`} hred={`/recipe/${recipe.id}`}>
         See More
       </Link>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import styles from './styles/index.css';
-
-// const routing = (
-//   <Router>
-//     <div>
-//       <Header />
-//       <Switch>
-//         <Redirect from="/home" to="/" />
-//         <Route exact path="/" component={Home} />
-//         <Route path="/favorites" component={Favorites} />
-//         <Route component={NotFound} />
-//       </Switch>
-//     </div>
-//   </Router>
-// );
+import './styles/index.css';
 
 const MOUNT_ELEMENT = document.getElementById('root');
-ReactDOM.render(<App/>, MOUNT_ELEMENT);
+ReactDOM.render(<App />, MOUNT_ELEMENT);
